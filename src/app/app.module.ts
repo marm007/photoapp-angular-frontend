0b625d4cfc77c1ee8f62a087bcc57685c1c66ec3
marm007 <dhkm.gornica@poczta.fm>
2020-03-17 18:45:29 +0100

    initial message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { PhotoComponent } from './photo/photo.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { PostsComponent } from './posts/posts.component';
+import { RelationsComponent } from './relations/relations.component';
+import {ScrollingModule} from '@angular/cdk/scrolling';
+import { TruncateModule } from '@yellowspot/ng-truncate';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PhotoComponent
+    PostsComponent,
+    RelationsComponent
   ],
   imports: [
     BrowserModule,
+    TruncateModule,
     HttpClientModule,
     BrowserAnimationsModule,
     MatCardModule,
@@ -24,6 +29,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
       useColumnBasisZero: false,
       printWithBreakpoints: ['md', 'lt-lg', 'lt-xl', 'gt-sm', 'gt-xs']
     }),
+    ScrollingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/photo.ts
+++
@@ -1,9 +0,0 @@
-import {User} from './user';
-
-export interface Photo {
-  id: number;
-  image: string;
-  likes: number;
-  description: string;
-  user: User;
-}
