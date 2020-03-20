b0fe5c49920e1dd90be1eadf607dc587ab9999ca
marm007 <dhkm.gornica@poczta.fm>
2020-03-20 11:27:34 +0100

    initial message

--- src/app/add-post/add-post.component.ts
+++ src/app/add-post/add-post.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {ImageService} from '../image.service';
+
+class ImageSnippet {
+  pending = false;
+  status = 'init';
+  constructor(public src: string, public file: File) {
+  }
+}
 
 @Component({
   selector: 'app-add-post',
@@ -7,9 +15,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddPostComponent implements OnInit {
 
-  constructor() { }
+  selectedFile: ImageSnippet;
+
+  imageAdded: boolean;
+
+  constructor(private imageService: ImageService) {
+    this.imageAdded = false;
+  }
 
   ngOnInit(): void {
+  }
+
+  private onSuccess() {
+    this.selectedFile.pending = false;
+    this.selectedFile.status = 'ok';
+  }
+
+  private onError() {
+    this.selectedFile.pending = false;
+    this.selectedFile.status = 'fail';
+    this.selectedFile.src = '';
+  }
+
+  processFile(imageInput: any) {
+    const file: File = imageInput.files[0];
+    const reader = new FileReader();
+
+    reader.addEventListener('load', (event: any) => {
+      this.selectedFile = new ImageSnippet(event.target.result, file);
+      this.selectedFile.pending = true;
+     /* this.imageService.uploadImage(this.selectedFile.file).subscribe(
+        (res) => {
+          this.onSuccess();
+        },
+        (err) => {
+          this.onError();
+        });*/
+    });
+
+    reader.readAsDataURL(file);
   }
 
 }
