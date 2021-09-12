391adf84e7f4d669d80603839d0b43fd95e24469
marm007 <dhkm.gornica@poczta.fm>
2021-09-12 22:04:14 +0200

    chore: backend url changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiURL: 'https://marm007-photo-app.herokuapp.com/api',
+  apiURL: 'https://marm007-insta-django-backend.herokuapp.com/api',
   mediaHttpURL: 'http://res.cloudinary.com/marm007/',
   mediaURL: 'https://res.cloudinary.com/marm007/',
   avatarURL: 'https://res.cloudinary.com/marm007/image/upload/v1585912225/avatar_iahjs2.png'
